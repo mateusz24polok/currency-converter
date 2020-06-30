7cd3f7ee90c617a88fea8fffded29b5ede926d12
Mateusz Polok <mateusz24polok@gmail.com>
2020-06-30 18:24:40 +0200

    delete unnecessary comments, correct small mistakes, correct variables name

--- js/script.js
+++ js/script.js
@@ -1,5 +1,3 @@
-//DOM Elements declaration//
-
 const firstCurrencySelectElement = document.querySelector(".js-firstCurrencySelect");
 const secondCurrencySelectElement = document.querySelector(".js-secondCurrencySelect");
 const firstCurrencyValueInputElement = document.querySelector(".js-firstCurrencyValueInputElement");
@@ -7,26 +5,22 @@ const secondCurrencyValueInputElement = document.querySelector(".js-secondCurren
 const calculateButtonElement = document.querySelector(".js-calculateButton");
 const exchangeDetailsMessageElement = document.querySelector(".js-message");
 
-//Async function witch returns rate value and data on the base of basedCurrency and exchangeCurrency frome extern currency convert API//
-
 const getExchangeData = async (basedCurrency, exchangedCurrency) => {
-    const exchangeDataPromise = await fetch(`https://api.exchangeratesapi.io/latest?base=${basedCurrency}`);
-    const exchangeData = await exchangeDataPromise.json();
+    const exchangeDataResponse = await fetch(`https://api.exchangeratesapi.io/latest?base=${basedCurrency}`);
+    const exchangeData = await exchangeDataResponse.json();
     //handle API EUR-EUR error
     if (basedCurrency === "EUR" && exchangedCurrency === "EUR") {
         return {
             exchangeRate: 1,
-            exchagneDate: exchangeData.date
+            exchangeDate: exchangeData.date
         }
     } else {
         return {
-            exchangeRate: exchangeData.rates[`${exchangedCurrency}`],
-            exchagneDate: exchangeData.date
+            exchangeRate: exchangeData.rates[exchangedCurrency],
+            exchangeDate: exchangeData.date
         }
     }
-}
-
-//Function witch shows You alert message and reset input fields, when You put negative value of currency//
+};
 
 const handleNegativeValue = () => {
     firstCurrencyValueInputElement.value = "";
@@ -37,9 +31,7 @@ const handleNegativeValue = () => {
     setTimeout(() => {
         exchangeDetailsMessageElement.classList.remove("calculatorForm__Message--warning")
     }, 1500)
-}
-
-//Functions witch updates first or second currency value
+};
 
 const updateFirstCurrencyValue = () => {
     //Reset old message text//
@@ -48,7 +40,7 @@ const updateFirstCurrencyValue = () => {
         .then(exchangeData => {
             firstCurrencyValueInputElement.value = (secondCurrencyValueInputElement.value * exchangeData.exchangeRate).toFixed(2);
         }).catch(err => console.log(err))
-}
+};
 
 const updateSecondCurrencyValue = () => {
     //Reset old message text//
@@ -57,19 +49,15 @@ const updateSecondCurrencyValue = () => {
         .then(exchangeData => {
             secondCurrencyValueInputElement.value = (firstCurrencyValueInputElement.value * exchangeData.exchangeRate).toFixed(2);
         }).catch(err => console.log(err))
-}
-
-//Function witch shows You details about exchange
+};
 
 const showDetailsMessage = () => {
     exchangeDetailsMessageElement.classList.add("calculatorForm__Message--info");
     getExchangeData(firstCurrencySelectElement.value, secondCurrencySelectElement.value)
         .then(exchangeData => {
-            exchangeDetailsMessageElement.textContent = `Your calculation is current for ${exchangeData.exchagneDate} and your exchange rate is ${exchangeData.exchangeRate.toFixed(2)}`;
+            exchangeDetailsMessageElement.textContent = `Your calculation is current for ${exchangeData.exchangeDate} and your exchange rate is ${exchangeData.exchangeRate.toFixed(2)}`;
         }).catch(err => console.log(err))
-}
-
-//Function with shows You details about exchange
+};
 
 const handleCurrencyValueInputChange = (currencyValueInputElement) => {
     if (currencyValueInputElement.value >= 0) {
@@ -77,9 +65,7 @@ const handleCurrencyValueInputChange = (currencyValueInputElement) => {
     } else if (currencyValueInputElement.value < 0) {
         handleNegativeValue();
     }
-}
-
-//ADD EVENT LISTENERS//
+};
 
 firstCurrencySelectElement.addEventListener("change", updateSecondCurrencyValue)
 
